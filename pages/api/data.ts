ea963164be1e4687e34b12f196e1d7401208f168
Mihir <aminvasudev6@gmail.com>
2024-06-03 22:15:43 +0530

    waybill v1.1.0 Client Dashboard

---
+++ pages/api/data.ts
@@ -0,0 +1,21 @@
+// pages/api/data.ts
+import { MongoClient } from 'mongodb';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === 'GET') {
+    const client = await MongoClient.connect("mongodb+srv://aminvasudev6:wcw9QsKgW3rUeGA4@waybillcluster.88jnvsg.mongodb.net/?retryWrites=true&w=majority&appName=waybillCluster");
+    const db = client.db(process.env.DB_NAME);
+
+    const data = await db.collection('waybills').find().toArray();
+    client.close();
+    console.log(data);
+
+    res.status(200).json(data);
+  } else {
+    res.status(405).json({ message: 'Only GET requests are allowed' });
+  }
+}
--- pages/client.tsx
+++ pages/client.tsx
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import styles from "./client.module.css";
 
-export default function clientPage() {
-    return (
-      <div>
-        <h1>Hey Client</h1>
-        <table className={styles.table}>
-          <thead>
-            <tr>
-              <th>Serial Number</th>
-              <th>Sender Name</th>
-              <th>Waybill Number</th>
-              <th>No of Documents</th>
-              <th>Weight</th>
-              <th>Courier Partner</th>
+interface DataItem {
+  serialNumber: string;
+  sender: string;
+  waybillNo: string;
+  noOfDocuments: string;
+  weight: string;
+  courierCompany: string;
+}
+
+export default function ClientPage() {
+  const [data, setData] = useState<DataItem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get<DataItem[]>('/api/data');
+      setData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hey Client</h1>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Serial Number</th>
+            <th>Sender Name</th>
+            <th>Waybill Number</th>
+            <th>No of Documents</th>
+            <th>Weight</th>
+            <th>Courier Partner</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td>{item.serialNumber}</td>
+              <td>{item.sender}</td>
+              <td>{item.waybillNo}</td>
+              <td>{item.noOfDocuments}</td>
+              <td>{item.weight}</td>
+              <td>{item.courierCompany}</td>
             </tr>
-          </thead>
-          <tbody>
-            
-          </tbody>
-        </table>
-      </div>
-    );
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
