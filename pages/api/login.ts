a55a2aa6d9d070bfc4d4cd9df1bbfb3cf0829cc4
Mihir <aminvasudev6@gmail.com>
2024-06-03 21:19:44 +0530

    waybill v1.0.1

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,3 +1,42 @@
+// // pages/api/login.ts
+// import { MongoClient } from 'mongodb';
+// import type { NextApiRequest, NextApiResponse } from 'next';
+// import bcrypt from 'bcryptjs';
+// import jwt from 'jsonwebtoken';
+
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === 'POST') {
+//     const { email, password } = req.body;
+
+//     const client = await MongoClient.connect("mongodb+srv://aminvasudev6:wcw9QsKgW3rUeGA4@waybillcluster.88jnvsg.mongodb.net/?retryWrites=true&w=majority&appName=waybillCluster");
+//     const db = client.db(process.env.DB_NAME);
+
+//     const existingUser = await db.collection('users').findOne({ email });
+//     if (!existingUser) {
+//       res.status(400).json({ message: 'User does not exist' });
+//       client.close();
+//       return;
+//     }
+
+//     const passwordIsValid = await bcrypt.compare(password, existingUser.password);
+//     if (!passwordIsValid) {
+//       res.status(400).json({ message: 'Invalid password' });
+//       client.close();
+//       return;
+//     }
+
+//     const token = jwt.sign({ id: existingUser._id }, 'secret');
+
+//     client.close();
+
+//     res.status(200).json({ token });
+//   } else {
+//     res.status(405).json({ message: 'Only POST requests are allowed' });
+//   }
+// }
 // pages/api/login.ts
 import { MongoClient } from 'mongodb';
 import type { NextApiRequest, NextApiResponse } from 'next';
@@ -28,8 +67,8 @@ export default async function handler(
       return;
     }
 
-    const token = jwt.sign({ id: existingUser._id }, 'secret');
-
+    const token = jwt.sign({ id: existingUser._id }, "d2ea46a961f8222c5f3c13fa6be8b547c3f6461ed6881d3076428579eb14a9f9");
+    console.log(token + " "+  email + password)
     client.close();
 
     res.status(200).json({ token });
--- pages/client.tsx
+++ pages/client.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import styles from "../src/app/page.module.css";
+import styles from "./client.module.css";
 
 export default function clientPage() {
     return (
       <div>
-        hey Client
+        <h1>Hey Client</h1>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Serial Number</th>
+              <th>Sender Name</th>
+              <th>Waybill Number</th>
+              <th>No of Documents</th>
+              <th>Weight</th>
+              <th>Courier Partner</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+          </tbody>
+        </table>
       </div>
     );
-  }
+}
